de08dac2f53bd55cd9ea34649ca76846e420d068
fatuk <fatukk@gmail.com>
2013-04-07 17:42:22 +0400

    added to footer position:relative, fixed bug of hiding footer after toggle auth

--- homework/hw_1/js/script.js
+++ homework/hw_1/js/script.js
@@ -1,13 +1,11 @@
 function showAuth(){
 	document.getElementsByClassName('auth')[0].setAttribute('style', 'display:block;');
 	document.getElementsByClassName('blur-area')[0].setAttribute('style', '-webkit-filter: blur(4px) contrast(50%);');
-	// document.getElementsByClassName('blur-area')[0].setAttribute('class', 'blur-area blur');
 	return false;
 }
 
 function hideAuth(){
 	document.getElementsByClassName('auth')[0].setAttribute('style', 'display:none;');
 	document.getElementsByClassName('blur-area')[0].setAttribute('style', '-webkit-filter: none;');
-	// document.getElementsByClassName('blur-area')[0].setAttribute('class', 'blur-area');
 	return false;
 }
