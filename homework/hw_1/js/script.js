3346ff26e24a352852fcf933dc473c55c2bbdc98
Андрей Фатюк <fatukk@HP-620.(none)>
2013-04-14 21:40:10 +0400

    fixed popup auth in js file, add div under auth

--- homework/hw_1/js/script.js
+++ homework/hw_1/js/script.js
@@ -1,11 +1,13 @@
 function showAuth(){
 	document.getElementsByClassName('auth')[0].setAttribute('style', 'display:block;');
+	document.getElementsByClassName('shade')[0].setAttribute('style', 'display:block;');
 	document.getElementsByClassName('blur-area')[0].setAttribute('style', '-webkit-filter: blur(4px) contrast(50%);');
 	return false;
 }
 
 function hideAuth(){
 	document.getElementsByClassName('auth')[0].setAttribute('style', 'display:none;');
+	document.getElementsByClassName('shade')[0].setAttribute('style', 'display:none;');
 	document.getElementsByClassName('blur-area')[0].setAttribute('style', '-webkit-filter: none;');
 	return false;
 }
