555d5a9c56ce19f55d08cccfdd18049ebcd38684
fatuk <fatukk@gmail.com>
2013-05-13 23:18:24 +0400

    Ready news layout

---
+++ homework/hw_1/js/user-menu-logic.js
@@ -0,0 +1,50 @@
+function myToggle(selector){
+	if (selector === undefined) {
+		console.log("Вы не указали селектор");
+	}
+
+	var pure_selector = selector.substring(1, selector.length);
+	var attr = document.getElementsByClassName(pure_selector)[0].getAttribute('style');
+
+	if (attr === null || attr == "display:none;") {
+		show(selector);
+	}else{
+		hide(selector);
+	}
+}
+
+function show(selector){
+	if (selector === undefined) {
+		console.log("Вы не указали селектор");
+	}
+
+	if (selector.charAt(0) == ".") {
+		selector = selector.substring(1, selector.length);
+		document.getElementsByClassName(selector)[0].setAttribute('style', 'display:block;');
+	}
+
+	if (selector.charAt(0) == "#") {
+		selector = selector.substring(1, selector.length);
+		document.getElementsById(selector)[0].setAttribute('style', 'display:block;');
+	}
+
+	return false;
+}
+
+function hide(selector){
+	if (selector === undefined) {
+		console.log("Вы не указали селектор");
+	}
+
+	if (selector.charAt(0) == ".") {
+		selector = selector.substring(1, selector.length);
+		document.getElementsByClassName(selector)[0].setAttribute('style', 'display:none;');
+	}
+
+	if (selector.charAt(0) == "#") {
+		selector = selector.substring(1, selector.length);
+		document.getElementsById(selector)[0].setAttribute('style', 'display:none;');
+	}
+	
+	return false;
+}
